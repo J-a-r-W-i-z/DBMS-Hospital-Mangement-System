6f28b0686bfbb9bf78a099a65e30c028b0ff7289
Souvik Rana <ranasouvik07@gmail.com>
2023-02-28 12:00:00 +0530

    initial commit to souvik

--- SASSY/Front_End/src/index.js
+++ SASSY/Front_End/src/index.js
@@ -1,4 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import HomePage from './components/Homepage/Homepage.js'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-	<h1>Test app</h1>
+	<HomePage />
 )

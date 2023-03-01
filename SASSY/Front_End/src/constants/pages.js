90ddee12fd45cdbc0de2413eb13eed92d3c352f7
Geeky Quentin <pagotiyashwantkrishna@gmail.com>
2023-03-01 13:42:45 +0530

    update

--- SASSY/Front_End/src/constants/pages.js
+++ SASSY/Front_End/src/constants/pages.js
@@ -2,7 +2,9 @@ import { LoginForm, DoctorDashboard } from "../pages"
 
 const pages = [
   { name: "Home", path: "/", element: LoginForm },
-  { name: "Doctor Dashboard", path: "/doctor-dashboard", element: DoctorDashboard }
+  { name: "Doctor Dashboard", path: "/doctor-dashboard", element: DoctorDashboard },
+  { name: "Adminstrator", path: "/admin", element: DoctorDashboard }
+
 ]
 
 export { pages }

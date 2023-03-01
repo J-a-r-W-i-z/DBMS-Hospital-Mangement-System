a122a2e7460b969661cdab5d37941c15c7a9ee5e
Geeky Quentin <pagotiyashwantkrishna@gmail.com>
2023-03-01 14:20:04 +0530

    update

---
+++ SASSY/Front_End/src/constants/index.js
@@ -0,0 +1,2 @@
+export { pages } from "./pages.js"
+export { default as images } from "./images.jsx"
--- SASSY/Front_End/src/constants/index.jsx
+++
@@ -1,2 +0,0 @@
-export { pages } from "./pages.js"
-export { default as images } from "./images.js"

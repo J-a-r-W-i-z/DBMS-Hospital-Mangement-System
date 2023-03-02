ae68e24706c6e7a39f83d09a813dc0f4d004a30c
Geeky Quentin <pagotiyashwantkrishna@gmail.com>
2023-03-02 16:46:41 +0530

    created table component

---
+++ SASSY/Front_End/src/pages/404/404.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+import "./404.scss"
+
+const Error404 = () => {
+  return <div>404</div>
+}
+
+export default Error404
--- SASSY/Front_End/src/pages/Admin/Admin.jsx
+++ SASSY/Front_End/src/pages/Admin/Admin.jsx
@@ -1,9 +1,47 @@
-import React from "react"
+import React, { useState } from "react"
+import { Table } from "../../components"
+
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs"
 import "react-tabs/style/react-tabs.css"
 import "./Admin.scss"
 
 const AdminDashboard = () => {
+  const [users, setUsers] = useState([
+    {
+      username: "johnsnow",
+      name: "John Snow",
+      date_joined: "2021-01-01",
+    },
+    {
+      username: "janesmith",
+      name: "Jane Smith",
+      date_joined: "2021-01-01",
+    },
+    {
+      username: "bobjohnson",
+      name: "Bob Johnson",
+      date_joined: "2021-01-01",
+    },
+  ])
+
+  // function combineName(user) {
+  //   setUsers(
+  //     users.map((user) => {
+  //       user.name = user.first_name + " " + user.last_name
+  //       delete user.first_name
+  //       delete user.last_name
+  //       return user
+  //     })
+  //   )
+
+  //   console.log(users)
+  // }
+
+  function removeUser(username) {
+    setUsers(users.filter((user) => user.username !== username))
+    // TODO: Remove user from database
+  }
+
   return (
     <div className="section s-wrapper">
       <p className="primary-heading center-text">Admin Dashboard</p>
@@ -21,7 +59,19 @@ const AdminDashboard = () => {
           <Tab>Adminstrators</Tab>
         </TabList>
 
-        <TabPanel></TabPanel>
+        <div className="margin-divider-sm" />
+
+        <TabPanel>
+          <Table
+            headers={["Username", "Name", "Date Joined", "Action"]}
+            data={users}
+            searchKey="username"
+            handleClick={removeUser}
+            buttonLabel="Remove"
+            buttonClass="btn-secondary-sm"
+            clickKey="username"
+          />
+        </TabPanel>
         <TabPanel></TabPanel>
         <TabPanel></TabPanel>
         <TabPanel></TabPanel>

c635a7b8404d9e67de2be644642a95d18f162379
Anuj <anujkakde24@gmail.com>
2023-03-06 22:00:50 +0530

    appointments

--- SASSY/HM_System/urls.py
+++ SASSY/HM_System/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path
-from .views import RegisterView, LoginView, UserView, LogoutView, DoctorView, isAuth, PatientStayView, CreateUserView,InsertPatientView,InsertStayView,ConfirmAppointmentView,GetAdmittedView, GetUserProfile, DeleteUserView,GetPatientsView
-from .views import DischargePatientView
-
+from .views import *
 urlpatterns = [
     path('home', RegisterView.as_view()),
     path('login', LoginView.as_view()),
@@ -18,7 +16,8 @@ urlpatterns = [
     path('list-users', GetUserProfile.as_view()),
     path('delete-user', DeleteUserView.as_view()),
     path('list-patients', GetPatientsView.as_view()),
-    path('dischargePatient',DischargePatientView.as_view())
+    path('dischargePatient',DischargePatientView.as_view()),
+    path('list-appointments',UpcomingAppointments.as_view())
     # path('getProfile',), # get a request and send profile info for any user
     # path('insertPrescription',), # patient id,name,appointemt id,date  get appointment id,medication list and insert in table
     # path('insertTest',), # 
